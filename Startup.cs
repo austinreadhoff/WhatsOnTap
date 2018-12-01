2bdaa88112cf2767b03c07b79c382ab0a0878979
Austin <aread1800@gmail.com>
2018-12-01 18:28:31 -0500

    Added index.html fallback thingy

--- Startup.cs
+++ Startup.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -43,6 +45,12 @@ namespace WhatsOnTap
             }
 
             app.UseMvc();
+
+            app.Run( async (context) =>
+            {
+                context.Response.ContentType = "text/html";
+                await context.Response.SendFileAsync(Path.Combine(env.WebRootPath,"index.html"));
+            });
         }
     }
 }
